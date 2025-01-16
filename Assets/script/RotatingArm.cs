1deeff13a766d1d58d93c089e77b550b54310061
Daniel Hamilton <hamilt25@shernet.sheridancollege.ca>
2025-01-16 14:02:59 -0500

    Rotatept2

--- Assets/script/RotatingArm.cs
+++ Assets/script/RotatingArm.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class RotatingArm : MonoBehaviour
 {
+    float armspeed = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        armseped += 0
-        Vector2 clockarm = armspeed
-        transform.position = clockarm;
+        Quaternion rot = transform.rotation;
+        rot.z -= armspeed;
+        transform.rotation = rot;
+        if (rot.z == -180 )
+        {
+            rot.z = rot.z*-1;
+        }
     }
 }
